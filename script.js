66c2a5a888fdb74956d5780b48b0bad0e3ffa7cb
kyateka2023 <kyatekagivenethan@gmail.com>
2023-11-11 17:41:57 +0300

    Normaly committed

---
+++ script.js
@@ -0,0 +1,7 @@
+//We are going to style the image using javascript
+document.getElementById("me").style.width = "250px";
+document.getElementById("me").style.height = "250px";
+
+//we are going to space the magnifying glass
+document.querySelector(".fa-magnifying-glass").style.marginRight = "1rem";
+
